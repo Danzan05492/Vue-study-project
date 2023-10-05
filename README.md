5de89b2c8028912f1bb11382bd7711e316db2540
Danzan05492 <danzanius@bk.ru>
2023-10-05 12:31:39 +0800

    new reade

--- README.md
+++ README.md
@@ -24,8 +24,5 @@ npm run lint
 See [Configuration Reference](https://cli.vuejs.org/config/).
 
 ### Video
-<<<<<<< HEAD
-39 min https://www.youtube.com/watch?v=XzLuMtDelGk
-=======
 1h08m min https://www.youtube.com/watch?v=XzLuMtDelGk
->>>>>>> 38431b2 (add components and library for global export of components)
+### Дгиыфт пфн
--- src/main.js
+++ src/main.js
@@ -1,9 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-<<<<<<< HEAD
-
-createApp(App).mount('#app')
-=======
 import components from './components/UI';
 const app = createApp(App)
 
@@ -11,4 +7,3 @@ components.forEach(component =>{
     app.component(component.name, component)
 })
 app.mount('#app')
->>>>>>> 38431b2 (add components and library for global export of components)
